f7cc3f95bc4fb91a6e54e3770c7376621abbe122
Marcin Pazowski <pazowskimarcin@gmail.com>
2018-11-11 10:45:38 +0100

    https://www.youtube.com/watch?v=YrWQsuDT3NE&index=4&list=PLhfAbcv9cehhkG7ZQK0nfIGJC_C-wSLrx

--- Arcanoid2/Game.cpp
+++ Arcanoid2/Game.cpp
@@ -1,6 +1,7 @@
 #include "Game.h"
 
-
+SDL_Texture* playerTex;
+SDL_Rect srcRe, destR;
 
 Game::Game()
 {
@@ -41,6 +42,10 @@ void Game::init(const char * title, int xpos, int ypos, int width, int height, b
 	else {
 		isRunning = false;
 	}
+
+	SDL_Surface* tmpSurface = IMG_Load("assets/player.png");
+	playerTex = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+	SDL_FreeSurface(tmpSurface);
 }
 
 void Game::handleEvents()
@@ -60,13 +65,17 @@ void Game::handleEvents()
 void Game::update()
 {
 	cnt++;
+	destR.h = 64;
+	destR.w = 64;
+	destR.x = cnt;
+
 	std::cout << cnt << std::endl;
 }
 
 void Game::render()
 {
 	SDL_RenderClear(renderer);
-	// this is where we would add stuff to render
+	SDL_RenderCopy(renderer, playerTex, NULL, &destR);
 	SDL_RenderPresent(renderer);
 }
 
