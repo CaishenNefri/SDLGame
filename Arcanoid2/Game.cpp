d1efd01f5bb3a4fa77a3fd4e42cd77be83713567
Marcin Pazowski <pazowskimarcin@gmail.com>
2018-11-12 23:18:32 +0100

    https://www.youtube.com/watch?v=jq3Bg7XXjWw&list=PLhfAbcv9cehhkG7ZQK0nfIGJC_C-wSLrx&index=10

--- Arcanoid2/Game.cpp
+++ Arcanoid2/Game.cpp
@@ -1,19 +1,14 @@
 #include "Game.h"
 #include "TextureManager.h"
-#include "GameObject.h"
 #include "Map.h"
-
-#include "ECS.h"
 #include "Components.h"
 
-GameObject* player;
-GameObject* enemy;
 Map* map;
 
 SDL_Renderer* Game::renderer = nullptr;
 
 Manager manager;
-auto& newPlayer(manager.addEntity());
+auto& player(manager.addEntity());
 
 Game::Game()
 {
@@ -55,12 +50,11 @@ void Game::init(const char * title, int xpos, int ypos, int width, int height, b
 		isRunning = false;
 	}
 
-	player = new GameObject("assets/player.png",0,0);
-	enemy = new GameObject("assets/enemy.png", 50, 50);
+	
 	map = new Map();
 
-	newPlayer.addComponent<PositionComponent>();
-	newPlayer.getComponent<PositionComponent>().setPos(500, 500);
+	player.addComponent<PositionComponent>();
+	player.addComponent<SpriteComponent>("assets/player.png");
 }
 
 void Game::handleEvents()
@@ -79,19 +73,20 @@ void Game::handleEvents()
 
 void Game::update()
 {
-	player->Update();
-	enemy->Update();
+	manager.refresh();
 	manager.update();
-	std::cout << newPlayer.getComponent<PositionComponent>().x() << "," <<
-		newPlayer.getComponent<PositionComponent>().y() << std::endl;
+
+	if (player.getComponent<PositionComponent>().x() > 100)
+	{
+		player.getComponent<SpriteComponent>().setTex("assets/enemy.png");
+	}
 }
 
 void Game::render()
 {
 	SDL_RenderClear(renderer);
 	map->DrawMap();
-	player->Render();
-	enemy->Render();
+	manager.draw();
 	SDL_RenderPresent(renderer);
 }
 
